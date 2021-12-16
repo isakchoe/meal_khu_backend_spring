1813e831cf2991f2507e6c4da06f877d3948d0c1
최부성 <isakchoe@naver.com>
2021-12-16 22:33:57 +0900

    java version setting

--- src/main/java/com/example/mealkhu2/controller/SeoulController.java
+++ src/main/java/com/example/mealkhu2/controller/SeoulController.java
@@ -3,14 +3,13 @@ package com.example.mealkhu2.controller;
 import com.example.mealkhu2.document.SeoulRest;
 import com.example.mealkhu2.repository.MongoDbRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-//@Controller
+
 @RestController
 public class SeoulController {
 
